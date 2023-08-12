9d01e61ad3635e46f574a5407084bd210b0d1a9a
zyiloveyou010321117 <yingzhou.huo@gtiit.edu.cn>
2023-08-12 14:18:06 +0800

    a better way to give the n th element of fibonacci sequence

---
+++ a better way to give the n th element of fibonacci sequence/a better way to give the n th element of fibonacci sequence/a better way to give the n th element of fibonacci sequence.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int fib(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	while (n > 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
+}
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = fib(n);
+	printf("%d", ret);
+	return 0;
+}
