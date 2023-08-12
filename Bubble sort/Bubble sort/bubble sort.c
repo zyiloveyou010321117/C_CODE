4236423b696ede2a97a70065743c2e513c773dfa
zyiloveyou010321117 <yingzhou.huo@gtiit.edu.cn>
2023-08-12 20:56:05 +0800

    Bubble sort

---
+++ Bubble sort/Bubble sort/bubble sort.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void bubble_sort(int arr[],int sz)
+{
+	//how many times of sorts?
+	int i = 0;
+	for (i = 0; i < sz-1; i++)
+	{
+		//how many pairs of numbers to compare in one sort?
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				int tem = 0;
+				tem = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tem;
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[] = { 32,42,63,79,12,7,31,63,68,98 };
+	int n = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz);
+	for (n = 0; n < sz; n++)
+	{
+		printf("%d ", arr[n]);
+	}
+	
+	return 0;
+}
